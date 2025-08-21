c59a9fa956821119d93f480c44438825a8aa6fec
Immun4ik <mr.lehabear@mail.ru>
2025-08-21 21:57:33 +0300

    README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Домашнее задание к занятию "8. EventSource, Websockets" (frontend)
 
+[![Build status](https://ci.appveyor.com/api/projects/status/ryvlvok62ra50ch5?svg=true)](https://ci.appveyor.com/project/immun4ik/ahj-websockets-frontend)
+
+
--- src/js/Controller.js
+++ src/js/Controller.js
@@ -15,13 +15,13 @@ export default class Controller {
   }
 
   async init() {
-    this.spinner = new Spinner(); 
-    const server = await Service.pingServer(); 
-    this.spinner.removeSpinner(); 
+    this.spinner = new Spinner(); // запускаем спиннер ожидания
+    const server = await Service.pingServer(); // ждём ответа от сервера
+    this.spinner.removeSpinner(); // убираем спиннер после получения ответа от сервера
 
     // обработка ошибки подключения к серверу:
     if (server.status === 520) {
-      this.serverError = new ServerError(server.status); 
+      this.serverError = new ServerError(server.status); // отрисовать ошибку сервера
       return;
     }
 
@@ -29,7 +29,7 @@ export default class Controller {
   }
 
   renderModal() {
-    this.modal = new Modal(); 
+    this.modal = new Modal(); // отрисовка модального окна для запроса никнейма юзера
     this.modal.submitEvent(this.addModalSubmitEvent.bind(this));
   }
 
@@ -46,30 +46,32 @@ export default class Controller {
 
     const data = await Service.registerUser(name); // попытка регистрации юзера на сервере
 
-    
+    // ---------- ошибка сервера при отправке данных: ----------
+    // data: { error: true, status: 520 };
     if (data.error) {
       this.modal.removeForm(); // удалить модалку из DOM
       this.serverError = new ServerError(data.status); // отрисовать ошибку сервера
       return;
     }
 
-    
+    // -------------------- занятый никнейм: -------------------
+    // data: { status: "error", message: "This name is already taken!" }
     if (data.status === 'error') {
-      this.modal.showTooltip('Это имя уже занято! Выберите другое!'); 
-      setTimeout(() => this.modal.hideTooltip(), 1000 * 2); 
+      this.modal.showTooltip('Это имя уже занято! Выберите другое!'); // показать на 2 сек, что имя занято
+      setTimeout(() => this.modal.hideTooltip(), 1000 * 2); // убрать подсказку
       return;
     }
 
     // ------------------- работа с юзером!!! ------------------
-    
+    // data: { status: "ok", user { id: "...", name: "..." } }
     if (data.status === 'ok') {
       this.currentId = data.user.id; // свой id
       this.currentName = data.user.name; // своё имя
 
-      window.addEventListener('beforeunload', this.exit.bind(this)); 
+      window.addEventListener('beforeunload', this.exit.bind(this)); // перед закрытием страницы...
 
-      this.modal.removeForm(); 
-      this.renderPage(); 
+      this.modal.removeForm(); // удаляем модалку из DOM
+      this.renderPage(); // отрисовываем страницу чата
     }
   }
 
@@ -79,13 +81,13 @@ export default class Controller {
       user: { id: this.currentId, name: this.currentName },
     };
 
-    this.ws.send(JSON.stringify(msg)); 
+    this.ws.send(JSON.stringify(msg)); // отправка данных через ws-соединение
   }
 
   renderPage() {
-    this.container.classList.remove('hidden'); 
-    this.usersContainer = new Users(this.container); 
-    this.chatContainer = new Chat(this.container); 
+    this.container.classList.remove('hidden'); // отрисовка контейнера для всего контента
+    this.usersContainer = new Users(this.container); // отрисовка контейнера для юзеров
+    this.chatContainer = new Chat(this.container); // отрисовка контейнера для сообщений
 
     this.chatContainer.addSubmitEvent(this.addChatSubmitEvent.bind(this)); // 'submit'
 
@@ -96,29 +98,42 @@ export default class Controller {
     // this.ws = new WebSocket('ws://localhost:7070/ws'); // локальный сервер
     this.ws = new WebSocket('wss://ahj-websockets-backend.onrender.com/ws'); // сервер на Render
 
-   
+    // событие 'open' - возникает только 1 раз на каждой странице
+    // this.ws.addEventListener('open', (event) => {
+    //   console.log('ws open', event);
+    // });
 
+    // событие 'message' - при входе каждого нового юзера и при ws.send()
     this.ws.addEventListener('message', (event) => {
+      // data = [ { id: '...', name: '...' }, ... ] <- массив юзеров, если type !== 'send'
+      // data = { type: "send", msg: "...", user: {id: "...", name: "..."}, created: "..." }
       const data = JSON.parse(event.data);
 
       // отрисовка сообщений у всех юзеров:
       if (data.type === 'send') {
         const name = data.user.id === this.currentId ? 'You' : data.user.name;
         const info = `${name}, ${data.created}`;
-        this.chatContainer.addMessage(info, data.msg, name === 'You'); 
+        this.chatContainer.addMessage(info, data.msg, name === 'You'); // 'Julia, 20:50 19.09.2024', 'Hello!', true
         this.chatContainer.resetForm(); // очищаем форму
         return;
       }
 
       // обновление списка юзеров при входе/выходе каждого юзера:
-      this.usersContainer.deleteUsers(); 
+      this.usersContainer.deleteUsers(); // 1. полная очистка списка юзеров
 
       data.forEach((user) => {
         const name = user.id === this.currentId ? 'You' : user.name;
-        this.usersContainer.addUser(name); 
+        this.usersContainer.addUser(name); // 2. добавление заново всех юзеров, которые онлайн
       });
     });
 
+    // this.ws.addEventListener('error', (event) => {
+    //   console.error('ws error', event);
+    // });
+
+    // this.ws.addEventListener('close', (event) => {
+    //   console.warn('ws close', event);
+    // });
   }
 
   addChatSubmitEvent(event) {
@@ -127,7 +142,7 @@ export default class Controller {
     const message = this.chatContainer.getMessage();
 
     if (!message) {
-      this.chatContainer.resetForm(); 
+      this.chatContainer.resetForm(); // очищаем форму
       return;
     }
 
@@ -155,6 +170,6 @@ export default class Controller {
       created: formattedDate,
     };
 
-    this.ws.send(JSON.stringify(msg)); 
+    this.ws.send(JSON.stringify(msg)); // отправка данных через ws-соединение
   }
 }
